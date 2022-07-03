885f4d234f4fb65776b097cd0d3ebc497299ef8b
Acemir Sousa Mendes <acemirsm@gmail.com>
2022-07-03 17:57:08 +0100

    First commit

---
+++ precache-manifest.21b4f0e29f816b72d91376b6905711b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f904dabe96cdcd306dc0330b9ec79f00",
+    "url": "/50-curtas/index.html"
+  },
+  {
+    "revision": "e61f5bdef9542ebfab22",
+    "url": "/50-curtas/static/css/2.10703e33.chunk.css"
+  },
+  {
+    "revision": "8d1d3e7e5598c1b0ddf9",
+    "url": "/50-curtas/static/css/main.4b74e6e9.chunk.css"
+  },
+  {
+    "revision": "e61f5bdef9542ebfab22",
+    "url": "/50-curtas/static/js/2.45ad2330.chunk.js"
+  },
+  {
+    "revision": "8d1d3e7e5598c1b0ddf9",
+    "url": "/50-curtas/static/js/main.bd70aad7.chunk.js"
+  },
+  {
+    "revision": "16f852252d5c75be0ed5",
+    "url": "/50-curtas/static/js/runtime-main.4fd61085.js"
+  }
+]);
